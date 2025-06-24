a9d7542a63abce0c0577cb12a474aabec67bd372
msuarez28ofi <msuarez.6499@unimar.edu.ve>
2025-06-24 01:21:17 -0400

    Agregar archivos de proyecto.

---
+++ Reservas_padel/Login.cs
@@ -0,0 +1,20 @@
+namespace Reservas_padel
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
